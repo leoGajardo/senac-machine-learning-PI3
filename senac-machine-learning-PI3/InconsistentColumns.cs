175015fa3a17a8ae0570f5d8a40cf652a98c078a
LeoGajardo <leonardo@gajardo.com.br>
2018-03-04 16:48:50 -0300

    Adding function to normalize Data and draft to remove columns

---
+++ senac-machine-learning-PI3/InconsistentColumns.cs
@@ -0,0 +1,17 @@
+﻿using senac_machine_learning_PI3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace senac_machine_learning_PI3
+{
+    public static class InconsistentColumns
+    {
+        public static DataTable RemoveInconsistentColumns(this DataTable data)
+        {
+            return data;
+        }
+    }
+}
---
+++ senac-machine-learning-PI3/NormalizeData.cs
@@ -0,0 +1,28 @@
+﻿using senac_machine_learning_PI3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace senac_machine_learning_PI3
+{
+    public static class NormalizeData
+    {
+        public static DataTable NomalizeData(this DataTable data)
+        {
+            foreach (var column in data.Schema.Columns.Where(c => c.Value.Type != Column.ColumnType.Nominal))
+            {
+                var maxVal = data.Data.Max(d => double.Parse(d.Columns[column.Key]));
+                var minVal = data.Data.Min(d => double.Parse(d.Columns[column.Key]));
+                foreach (var line in data.Data)
+                {
+                    var val = double.Parse(line.Columns[column.Key]);
+                    var newVal = (val - minVal) / (maxVal - minVal);
+                    line.Columns[column.Key] = newVal.ToString();
+                }
+            }
+            return data;
+        }
+    }
+}
