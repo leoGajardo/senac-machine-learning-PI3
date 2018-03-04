4c7a030c70e358c084182cd7d85b4fe3083c3e57
LeoGajardo <leonardo@gajardo.com.br>
2018-03-04 00:49:39 -0300

    fixing the method to remove lines

--- senac-machine-learning-PI3/InconsistentLines.cs
+++ senac-machine-learning-PI3/InconsistentLines.cs
@@ -12,14 +12,17 @@ namespace senac_machine_learning_PI3
 
         public static DataTable RemoveInconsistentLines(this DataTable data)
         {
-
+            var shouldBeRemoved = new List<int>();
             foreach (var line in data.Data)
                 foreach (var value in line.Columns)
                     if (value.Trim().ToLower() == "NULL".ToLower())
-                        data.RemoveLine(line.Id);
+                        shouldBeRemoved.Add(line.Id);
+
+            //Necessário fazer separado, pois mexer no IEnumerable durante o foreach quebra o iterador
+            foreach (var id in shouldBeRemoved)
+                data.RemoveLine(id);
 
             return data;
         }
-
     }
 }
