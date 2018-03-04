81881e7623570a148024a861ad47cc7a0196ed28
GabrielPereiraPinto <arkanjogabriel7@hotmail.com>
2018-03-03 23:28:23 -0300

    Data Corrections and Tables Creation

--- senac-machine-learning-PI3/Program.cs
+++ senac-machine-learning-PI3/Program.cs
@@ -27,7 +27,25 @@ namespace senac_machine_learning_PI3
 
         private static void BuildSchemaTables()
         {
+
             tables = new List<Models.DataTable>();
+
+            var AbaloneColumns = new List<Column> {
+                new Column(){ Name = "Sex", Type = Column.ColumnType.Nominal, Enum = typeof(Enums.Abalone.Sex) } ,
+                new Column(){ Name = "Length", Type = Column.ColumnType.Continuous } ,
+                new Column(){ Name = "Diameter", Type = Column.ColumnType.Continuous } ,
+                new Column(){ Name = "Height", Type = Column.ColumnType.Continuous } ,
+                new Column(){ Name = "Whole weight", Type = Column.ColumnType.Continuous } ,
+                new Column(){ Name = "Shucked weight", Type = Column.ColumnType.Continuous } ,
+                new Column(){ Name = "Viscera weight", Type = Column.ColumnType.Continuous } ,
+                new Column(){ Name = "Shell weight", Type = Column.ColumnType.Continuous } ,
+                new Column(){ Name = "Rings", Type = Column.ColumnType.Integer }
+            };
+            var AbaloneSchema = new TableSchema(AbaloneColumns);
+            AbaloneSchema.TotalOfRecords = 4177;
+            tables.Add(new Models.DataTable("Data/abalone.csv", AbaloneSchema));
+
+
             var AdultColumns = new List<Column> {
                 new Column(){ Name = "age", Type = Column.ColumnType.Continuous } ,
                 new Column(){ Name = "workclass", Type = Column.ColumnType.Nominal, Enum = typeof(Enums.Adult.Workclass) } ,
@@ -48,6 +66,76 @@ namespace senac_machine_learning_PI3
             AdultSchema.TotalOfRecords = 32561;
 
             tables.Add(new Models.DataTable("Data/adult.csv", AdultSchema));
+
+            var BreastCancerColumns = new List<Column>
+            {
+                new Column(){ Name ="ID", Type=Column.ColumnType.Integer } ,
+                new Column(){ Name ="Diagonis", Type=Column.ColumnType.Nominal, Enum = typeof(Enums.BreastCancer.Diagnosis) } ,
+                new Column(){ Name ="Radius_Cell-1",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Texture_Cell-1",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Perimeter_Cell-1",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Area_Cell-1",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Smoothness_Cell-1",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Compactness_Cell-1",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Concavity_Cell-1",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Concave-Points_Cell-1",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Symmetry_Cell-1",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Fractal-Dimension_Cell-1",Type=Column.ColumnType.Continuous} ,
+
+                new Column(){ Name ="Radius_Cell-2",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Texture_Cell-2",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Perimeter_Cell-2",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Area_Cell-2",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Smoothness_Cell-2",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Compactness_Cell-2",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Concavity_Cell-2",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Concave-Points_Cell-2",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Symmetry_Cell-2",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Fractal-Dimension_Cell-2",Type=Column.ColumnType.Continuous} ,
+
+                new Column(){ Name ="Radius_Cell-3",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Texture_Cell-3",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Perimeter_Cell-3",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Area_Cell-3",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Smoothness_Cell-3",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Compactness_Cell-3",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Concavity_Cell-3",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Concave-Points_Cell-3",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Symmetry_Cell-3",Type=Column.ColumnType.Continuous} ,
+                new Column(){ Name ="Fractal-Dimension_Cell-3",Type=Column.ColumnType.Continuous} ,
+            };
+            var BreastCancerSchema = new TableSchema(BreastCancerColumns);
+            BreastCancerSchema.TotalOfRecords = 569;
+            tables.Add(new Models.DataTable("Data/BreastCancer.csv", BreastCancerSchema));
+
+            var IrisColumns = new List<Column>
+            {
+                new Column(){ Name ="Sepal-Lengh", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Sepal-Width", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Petal-Lengh", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Petal-Width", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Class", Type=Column.ColumnType.Nominal, Enum = typeof(Enums.Iris.IrisType) } ,
+            };
+            var IrisSchema = new TableSchema(IrisColumns);
+            IrisSchema.TotalOfRecords = 150;
+            tables.Add(new Models.DataTable("Data/Iris.csv", IrisSchema));
+
+            var Wine = new List<Column>
+            {
+                new Column(){ Name ="Alcohol", Type=Column.ColumnType.Integer } ,
+                new Column(){ Name ="Malic-Acid", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Ash", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Alcalinity-of-Ash", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Magnesium", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Total-Phenols", Type=Column.ColumnType.Integer } ,
+                new Column(){ Name ="Flavanoids", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="NonFlavanoids-Phenols", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Proanthocyanins", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Color-Intensity", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Hue", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="OD280-OD315-of-diluted-wines", Type=Column.ColumnType.Continuous } ,
+                new Column(){ Name ="Proline", Type=Column.ColumnType.Integer } ,
+            };
         }
     }
 }
