52c475b903d9a1be86cc65a58c7da4705a1329e2
LeoGajardo <leonardo@gajardo.com.br>
2018-03-04 00:36:04 -0300

    Adding file to remove inconsistent lines

---
+++ senac-machine-learning-PI3/InconsistentLines.cs
@@ -0,0 +1,25 @@
+﻿using senac_machine_learning_PI3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace senac_machine_learning_PI3
+{
+    public static class InconsistentLines
+    {
+
+        public static DataTable RemoveInconsistentLines(this DataTable data)
+        {
+
+            foreach (var line in data.Data)
+                foreach (var value in line.Columns)
+                    if (value.Trim().ToLower() == "NULL".ToLower())
+                        data.RemoveLine(line.Id);
+
+            return data;
+        }
+
+    }
+}
