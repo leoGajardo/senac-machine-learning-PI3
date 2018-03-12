a0d0577ace0e4a85c9bd72e6e9bb67c93df21dd7
GabrielPereiraPinto <arkanjogabriel7@hotmail.com>
2018-03-11 23:52:33 -0300

    ConvertEnum Fix

--- senac-machine-learning-PI3/Models/DataTable.cs
+++ senac-machine-learning-PI3/Models/DataTable.cs
@@ -47,7 +47,7 @@ namespace senac_machine_learning_PI3.Models
 
         public void ConvertEnums()
         {   
-            foreach (var column in Schema.Columns.Where(c => c.Value.Type == Column.ColumnType.Nominal))
+            foreach (var column in Schema.Columns.Where(c => c.Value.Type == Column.ColumnType.Nominal || c.Value.Type == Column.ColumnType.Class))
             {
                 for (int i = 0; i < Schema.TotalOfRecords; i++)
                 {
