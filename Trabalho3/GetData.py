c89504982c25580eb0d8bb28ab2e851b6e017345
Leo Gajardo <leonardo@gajardo.com.br>
2018-05-14 22:12:16 -0300

    improving somethings

--- Trabalho3/GetData.py
+++ Trabalho3/GetData.py
@@ -1,43 +1,46 @@
 import numpy as np
-import cv2
 import os
+import h5py
+from tflearn.data_utils import build_hdf5_image_dataset
+
+def build_dataset():
+    image_root_folder = "./Raw_DataSets/"
+    build_hdf5_image_dataset(image_root_folder, image_shape=(32, 32), mode='folder', output_path='dataset.h5', categorical_labels=True, normalize=True, grayscale=True)
+
+def generating_model():
+    # Load HDF5 dataset
+    h5f = h5py.File('dataset.h5', 'r')
+    
+    # Don't what is happening here
+    X = h5f['X']
+    Y = h5f['Y']
+
+    # Network layers
+    # model = DNN(network, ...)
+    model.fit(X, Y)
+    return model
 
 
-def get_data():
-    dataSet_path = os.listdir("./Raw_DataSets")
-    data = []
-    for folder in dataSet_path:
-        for image in os.listdir("./Raw_DataSets/" + folder):
-            image_path = "./Raw_DataSets/" + folder + "/" + image
-            # Load an color image in grayscale
-            image_data = cv2.imread("image_path" , 0 )
-            data.append(image_data,get_classe(image))
+    # Save a model
+    # model.save('my_model.tflearn')
+    # Load a model
+    # model.load('my_model.tflearn')
 
-    return data
+def trainModel():
+    # Set training mode ON (set is_training var to True)
+    # tflearn.is_training(True)
+    # Set training mode OFF (set is_training var to False)
+    # tflearn.is_training(False)
+    # trainop = TrainOp(net=my_network, loss=loss, metric=accuracy)
+    # model = Trainer(trainops=trainop, tensorboard_dir='/tmp/tflearn')
+    # model.fit(feed_dict={input_placeholder: X, target_placeholder: Y})
+    return ""
 
+def predict():
+    # model = Evaluator(network)
+    # model.predict(feed_dict={input_placeholder: X})
+    return ""
+    
 
-
-def get_classe(fileName):
-    classe = fileName.split("_")[0]
-    if classe == "CLASS0":
-        return np.array([1,0,0,0,0,0,0,0,0,0])
-    elif classe == "CLASS1":
-        return np.array([0,1,0,0,0,0,0,0,0,0])
-    elif classe == "CLASS2":
-        return np.array([0,0,1,0,0,0,0,0,0,0])
-    elif classe == "CLASS3":
-        return np.array([0,0,0,1,0,0,0,0,0,0])
-    elif classe == "CLASS4":
-        return np.array([0,0,0,0,1,0,0,0,0,0])
-    elif classe == "CLASS5":
-        return np.array([0,0,0,0,0,1,0,0,0,0])
-    elif classe == "CLASS6":
-        return np.array([0,0,0,0,0,0,1,0,0,0])
-    elif classe == "CLASS7":
-        return np.array([0,0,0,0,0,0,0,1,0,0])
-    elif classe == "CLASS8":
-        return np.array([0,0,0,0,0,0,0,0,1,0])
-    else:
-        return np.array([0,0,0,0,0,0,0,0,0,1])
-            
+build_dataset()
 
